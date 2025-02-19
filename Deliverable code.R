b59bab9c45eba1ed8acbca092bfc4e7dd22c3418
DWMSep <duncan@barkly.co.za>
2025-02-19 16:52:53 +0200

    removed values that equal 0 that were skewign the plots

--- Deliverable code.R
+++ Deliverable code.R
@@ -1,21 +1,31 @@
 # packages install
+
 install.packages("tidyverse")
 install.packages("ggplot2")
 
 # libraries
+
 library(tidyverse)
 library(ggplot2)
 
 # Reading in csv
+
 Jp <- read.csv("R_Deliverable.csv")
+jp <- Jp
 
 # summary of the data with all the headings and characters
+
 Jp
 
 # tidying up the data
 # change the names in colour and add back to Jp
+
 Jp$colour[Jp$colour == "red - green"] <- "red_green"
 Jp$colour[Jp$colour == "green-green"] <- "green_green"
+
+# ignoring all 0.0 values
+
+Jp <- Jp %>% filter(temp_bef != 0.0, temp_aft != 0.0, temp_change != 0.0)
 
 # tidying the data and making the boxplot
 
@@ -28,5 +38,7 @@ Jp %>%
         geom_boxplot(aes(y = Temperature, x = colour)) + # making the boxplot to hae the temperature on the y axis and the colour on the x axis                          
         theme(axis.text.x = element_text(angle = 45, hjust = 1))                                                       +      
         facet_wrap(vars(variable), scales = "free") # facet_wrap to have the boxplot for each variable         
+
+
 
                       
