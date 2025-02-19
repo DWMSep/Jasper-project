039e8f0bfb1b98016c9c9f1b38317846caa5cb3a
DWMSep <duncan@barkly.co.za>
2025-02-19 17:58:46 +0200

    added an extra line of code to add a heading to the final graph

--- Deliverable code.R
+++ Deliverable code.R
@@ -1,24 +1,24 @@
-# packages install
+# packages install ----
 
 install.packages("tidyverse")
 install.packages("ggplot2")
+install.packages("rmarkdown")
+install.packages("knitr")
 
-# libraries
+# libraries ----
 
 library(tidyverse)
 library(ggplot2)
+library(rmarkdown)
+library(knitr)
 
-# Reading in csv
+# Reading in csv----
 
 Jp <- read.csv("R_Deliverable.csv")
 jp <- Jp
 
-# summary of the data with all the headings and characters
-
-Jp
-
-# tidying up the data
-# change the names in colour and add back to Jp
+# tidying up the data----
+## change the names in colour and add back to Jp
 
 Jp$colour[Jp$colour == "red - green"] <- "red_green"
 Jp$colour[Jp$colour == "green-green"] <- "green_green"
@@ -27,17 +27,22 @@ Jp$colour[Jp$colour == "green-green"] <- "green_green"
 
 Jp <- Jp %>% filter(temp_bef != 0.0, temp_aft != 0.0, temp_change != 0.0)
 
-# tidying the data and making the boxplot
+### tidying the data and making the boxplot----
 
 Jp %>%
   group_by(colour) %>%
     select(!c("pair", "number", "shoot", "temp_ctr", "wet_weight", "time", "shoot_flower", "dry_weight")) %>% # removing the columns that are not needed
       pivot_longer(cols = c("temp_bef", "temp_aft", "temp_change"),
-      names_to = "variable", values_to = "Temperature") %>% # making the data long            
+      names_to = "variable", values_to = "Temperature") %>% # making the data long          
+# add a heading to the boxplot
         ggplot() +     
-        geom_boxplot(aes(y = Temperature, x = colour)) + # making the boxplot to hae the temperature on the y axis and the colour on the x axis                          
+        geom_boxplot(aes(y = Temperature, x = colour)) + # making the boxplot to hae the temperature on the y axis and the colour on the x axis 
+        ggtitle("Boxplot of temperature change by colour") +
+        theme(plot.title = element_text(hjust=0.5))+
         theme(axis.text.x = element_text(angle = 45, hjust = 1))                                                       +      
         facet_wrap(vars(variable), scales = "free") # facet_wrap to have the boxplot for each variable         
+
+
 
 
 
